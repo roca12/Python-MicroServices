86419908fba65765eb89cd2b0262d599ed1da6f9
roca12 <roca230313@gmail.com>
2021-03-15 14:22:54 -0500

    Finalizada union de la bd con admin, mas la actualizacion, eliminacion y selecion unitaria de productos, se inicia con el modulo main en flask

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductViewSet
+from .views import ProductViewSet, UserAPIView
 
 urlpatterns = [
     path('products', ProductViewSet.as_view({
@@ -10,6 +10,7 @@ urlpatterns = [
     path('products/<str:pk>', ProductViewSet.as_view({
          'get': 'retrieve',
          'put': 'update',
-         'delete': ' destroy'
+         'delete': 'destroy'
          })),
+    path('user',UserAPIView.as_view())
 ]
