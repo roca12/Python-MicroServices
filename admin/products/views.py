7a804cdc96e0515b27473703303134aa59fb7f98
roca12 <roca230313@gmail.com>
2021-03-15 13:09:08 -0500

    Finalizado primer get y post de los productos

---
+++ admin/products/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):#api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+    def create(self, request):  # api/products
+        serializer= ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    def retrieve(self, request, pk=None):  # api/products/<str:id>
+        pass
+    
+    def update(self, request, pk=None):  # api/products/<str:id>
+        pass
+    
+    def destroy(self, request, pk=None):  # api/products/<str:id>
+        pass
